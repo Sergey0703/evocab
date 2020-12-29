87e1f75a849f72a84ad20a4e91d5992bbf9cd7e4
unknown <sergey070373@gmail.com>
2020-12-29 20:10:40 +0200

    'okRoute3'

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -1,32 +1,35 @@
-import React, {useState,useCallback} from 'react'
+import {useState, useCallback} from 'react'
 
-export const useHttp=()=>{
-    const[loading, setLoading]=useState(false)
-    const[error,setError]=useState(null)
-    const request =useCallback(async (url, method='GET',body=null,headers={})=>{
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
         setLoading(true)
-        try{
-            if(body){
-                body=JSON.stringify(body)
-                headers['Content-Type']='application/json'
+        try {
+            if (body) {
+                body = JSON.stringify(body)
+                headers['Content-Type'] = 'application/json'
             }
-              const response= await fetch(url,{method, body, headers})
-              const data=await response.json()
 
-             if(!response.ok){
-               throw new Error(data.message()||'Что-то пошло не так..')
-             }
+            const response = await fetch(url, {method, body, headers})
+            const data = await response.json()
 
-             setLoading(false)
+            if (!response.ok) {
+                throw new Error(data.message || 'Что-то пошло не так')
+            }
+
+            setLoading(false)
+
             return data
-         }catch(e){
-           setLoading(false)
-           setError(e.message)
+        } catch (e) {
+            setLoading(false)
+            setError(e.message)
             throw e
         }
+    }, [])
 
-        },[])
+    const clearError = useCallback(() => setError(null), [])
 
-    const clearError=()=>setError(null)
-    return(loading,request,error,clearError)
+    return { loading, request, error, clearError }
 }
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,38 +1,98 @@
-import React,{useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {useHttp} from '../hooks/http.hook'
+import {useMessage} from '../hooks/message.hook'
+import {AuthContext} from '../context/AuthContext'
 
-export const AuthPage =()=>{
-    const {loading, request}=useHttp()
-    let [form,setForm]=useState({email:'',password:''})
+export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const message = useMessage()
+    const {loading, request, error, clearError} = useHttp()
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
 
-    const changeHandler=event=>{
-        setForm=({...form,[event.target.name]:[event.target.value]})
+   useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
+
+    useEffect(() => {
+  //      window.M.updateTextFields()
+    }, [])
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
     }
 
-    const registerHandler=async()=>{
+    const registerHandler = async () => {
         try {
-         const data=await request('/api/auth/register','POST',{...form})
-            console.log('Data',data)
-        }catch(e){}
+            const data = await request('/api/auth/register', 'POST', {...form})
+            message(data.message)
+        } catch (e) {}
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            auth.login(data.token, data.userId)
+        } catch (e) {}
     }
 
     return (
-        <div>
-            Auth page
-            <div>
-             <input placeholder="Введите email" type="text" name="email" id="email" onChange={changeHandler} />
-             <input placeholder="Введите password" type="text" name="password" id="password" onChange={changeHandler}/>
+        <div >
+            <div >
+                <h1>Сократи Ссылку</h1>
+                <div >
+                    <div >
+                        <span className="card-title">Авторизация</span>
+                        <div>
 
-            </div>
-            <div>
-            <button stule={{marginRight: 10}}
-            disabled={loading}
-            > Войти</button>
-            <button onClick={registerHandler}
-            disabled={loading}
-            > Зарегистрироваться</button>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Введите email"
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    className="yellow-input"
+                                    value={form.email}
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="input-field">
+                                <input
+                                    placeholder="Введите пароль"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    className="yellow-input"
+                                    value={form.password}
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Пароль</label>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button
+                            className="btn yellow darken-4"
+                            style={{marginRight: 10}}
+                            disabled={loading}
+                            onClick={loginHandler}
+                        >
+                            Войти
+                        </button>
+                        <button
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Регистрация
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
-
 }
